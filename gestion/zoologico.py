1f2941b26a99e32f58fc0d33aaa8b74c584238d6
Jhorman-Shair <jhramirezhe@unal.edu.co>
2024-03-19 11:51:23 -0500

    preube a2
    vamos por fa

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,35 +1,32 @@
-class Zoologico:
-    def __init__(self,nombre,ubicacion):
-        self._nombre=nombre
-        self._ubicacion=ubicacion
-        self._zonas=[]
-    
-    def getZonas(self):
-        return self._zonas
-    
-    def agregarzonas(self,zona):
-        self.getZonas().append(zona)
-        
+class Zoologico():
+    def __init__(self, nombre, ubicacion):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = []
+
+    def agregarZonas(self, zona):
+        self._zonas.append(zona)
+
     def cantidadTotalAnimales(self):
-        a=0
-        for i in self.zonas:
-            a+=i.cantidadAnimales()
-        return a
+        cantidad = 0
+        for zona in self._zonas:
+            cantidad += zona.cantidadAnimales()
+        return cantidad
     
     def getNombre(self):
         return self._nombre
     
-    def setNombre(self,nombre):
-        self._nombre=nombre
-    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
     def getUbicacion(self):
         return self._ubicacion
     
-    def setUbicacion(self,ubicacion):
-        self._ubicacion=ubicacion
-    
-    def getZonas(self):
+    def setUbicacion(self, ubicacion):
+        self._ubicacion = ubicacion
+
+    def getZona(self):
         return self._zonas
     
-    def setZonas(self,zonas):
-        self._zonas=zonas
+    def setZona(self, zonas):
+        self._zonas = zonas
--- zooAnimales/mamifero.py
+++ zooAnimales/mamifero.py
@@ -1,41 +1,38 @@
 from zooAnimales.animal import Animal
 
 class Mamifero(Animal):
-    caballos=0
-    leones=0
-    listado=[]
-    def __init__(self,nombre,edad,habitat,genero,pelaje,patas):
-        super.__init__(nombre,edad,habitat,genero)
-        self._pelaje=pelaje
-        self._patas=patas
-        Mamifero.agregarListado(self)
-        
-    def agregarListado(cls,a):
-        cls.listado.append(a)
+    _listado = []
+    caballos = 0
+    leones = 0
     
+    def __init__(self, nombre, edad, habitat, genero, pelaje, patas):
+        super().__init__(nombre, edad, habitat, genero)
+        self._pelaje = pelaje
+        self._patas = patas
+        Mamifero._listado.append(self)
+
+    @classmethod
     def cantidadMamiferos(cls):
-        return (len(cls.listado))
+        return len(Mamifero._listado)
     
-    def crearCaballo(cls,nombre,edad,genero):
-        caballo=Mamifero(nombre,edad,"pradera",genero,True,4)
-        cls.caballos+=1
-        Mamifero.agregarlistado(caballo)
-        return caballo
-    
-    def crearLeon(cls,nombre,edad,genero):
-        leon=Mamifero(nombre,edad,"selva",genero,True,4)
-        cls.leones+=1
-        Mamifero.agregarlistado(leon)
-        return leon
-    
-    def getPelaje(self):
+    @classmethod
+    def crearCaballo(cls, nombre, edad, genero):
+        cls.caballos += 1 
+        return cls(nombre, edad, "pradera", genero, True, 4)
+
+    @classmethod
+    def crearLeon(cls, nombre, edad, genero):
+        cls.leones += 1 
+        return cls(nombre, edad, "selva", genero, True, 4)
+
+    def isPelaje(self):
         return self._pelaje
-    
-    def setPelaje(self,pelaje):
-        self._pelaje=pelaje
-        
+
+    def setPelaje(self, pelaje):
+        self._pelaje = pelaje
+
     def getPatas(self):
         return self._patas
     
-    def setPatas(self,patas):
-        self._patas=patas
+    def setPatas(self, patas):
+        self._patas = patas
